51350db8be678be268c2ff5bbab79108fa6aa3b5
WHR <msl0000023508@gmail.com>
2020-07-21 21:43:15 +0800

    Disable experimental features warning

--- src/common/ceph_context.cc
+++ src/common/ceph_context.cc
@@ -250,9 +250,11 @@ public:
     get_str_set(conf->enable_experimental_unrecoverable_data_corrupting_features,
 		cct->_experimental_features);
     ceph_spin_unlock(&cct->_feature_lock);
+#if 0
     if (!cct->_experimental_features.empty())
       lderr(cct) << "WARNING: the following dangerous and experimental features are enabled: "
 		 << cct->_experimental_features << dendl;
+#endif
   }
 };
 
